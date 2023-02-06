07db553aa268170314216f92bb23a4589623208d
Andres_a <andres.hoyos18@eia.edu.co>
2023-02-06 13:33:01 -0500

    .

---
+++ Pokemon/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using PokeApiNet;
+
+Console.WriteLine("Hello, World!");
+
+var client = new PokeApiClient();
+var pokemon = await client.GetResourceAsync<Pokemon>("lucario");
+Console.WriteLine(pokemon.Name);
+
+var species = await client.GetResourceAsync(pokemon.Species);
+
+Console.WriteLine(
+    $"The '{species.Name}' sprite-url: {pokemon.Sprites.FrontDefault}'\n" +
+    $"Is legendary: {species.IsLegendary}\n" +
+    $"Is Mythical: {species.IsMythical}");
