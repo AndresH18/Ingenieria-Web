e43d904d356da3369595adb6d3e490113f7c703a
Andres_a <andres.hoyos18@eia.edu.co>
2023-02-07 12:59:27 -0500

    Learned more of the api design

--- Pokemon/ConsoleApp1/Program.cs
+++ Pokemon/ConsoleApp1/Program.cs
@@ -2,16 +2,29 @@
 
 using PokeApiNet;
 
-Console.WriteLine("Hello, World!");
 
 var client = new PokeApiClient();
-var pokemon = await client.GetResourceAsync<Pokemon>("lucario");
-Console.WriteLine(pokemon.Name);
+var pokemon = await client.GetResourceAsync<Pokemon>("eevee");
+Console.WriteLine("Pokemon: {0}", pokemon.Name);
 Console.WriteLine("Default sprite: {0}", pokemon.Sprites.Other.OfficialArtwork.FrontDefault);
 
 var species = await client.GetResourceAsync(pokemon.Species);
 
-Console.WriteLine(
-    $"The '{species.Name}' sprite-url: {pokemon.Sprites.FrontDefault}'\n" +
-    $"Is legendary: {species.IsLegendary}\n" +
-    $"Is Mythical: {species.IsMythical}");
+Console.WriteLine($"""
+The '{species.Name}' sprite-url: {pokemon.Sprites.FrontDefault}'
+Is legendary: {species.IsLegendary}
+Is Mythical: {species.IsMythical}
+""");
+var evoChain = await client.GetResourceAsync(species.EvolutionChain);
+Console.WriteLine($"--{evoChain.Chain.Species.Name} evolves to:");
+
+evoChain.Chain.EvolvesTo.ForEach(EvolutionChainSpecies);
+
+void EvolutionChainSpecies(ChainLink chainLink)
+{
+    Console.WriteLine("Name = {0}", chainLink.Species.Name);
+    if (chainLink.EvolvesTo.Count != 0)
+    {
+        chainLink.EvolvesTo.ForEach(EvolutionChainSpecies);
+    }
+}
