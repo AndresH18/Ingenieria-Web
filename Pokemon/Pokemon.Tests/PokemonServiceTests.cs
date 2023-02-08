cdb10ba91695e1ec27f5878ca391cfa323981eb3
Andres_a <andres.hoyos18@eia.edu.co>
2023-02-08 13:30:59 -0500

    Created Tests proyect

---
+++ Pokemon/Pokemon.Tests/PokemonServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using Pokemon.Services;
+
+namespace Pokemon.Tests;
+
+public class PokemonServiceTests
+{
+    [Fact]
+    public async void Can_GetAllAsync()
+    {
+        // arrange
+        var list = new List<string>()
+        {
+            "Pikachu", "Charizar", "Lucario"
+        };
+
+        var mock = new Mock<IPokemonService>();
+        mock.Setup(m => m.GetAllAsync(It.IsAny<int>())).ReturnsAsync(list);
+
+        var service = mock.Object;
+        // act
+        var result = (await service.GetAllAsync()).ToArray();
+
+        // assert
+        Assert.NotNull(result);
+        Assert.NotEmpty(result);
+        Assert.True(result.Length == list.Count);
+    }
+}
