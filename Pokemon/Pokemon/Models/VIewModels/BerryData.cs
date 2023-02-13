cdec94b63cd4ef0dfc5ffc11d77f9edfd2894480
Andres_a <andres.hoyos18@eia.edu.co>
2023-02-13 13:34:22 -0500

    Added Berries attributes

--- Pokemon/Pokemon/Models/VIewModels/BerryData.cs
+++ Pokemon/Pokemon/Models/VIewModels/BerryData.cs
@@ -10,10 +10,14 @@ public class BerryData
         BerryName = berry.Name;
         ItemId = item.Id;
         ItemName = item.Name;
+        ItemCost = item.Cost;
+        Sprite = item.Sprites.Default;
     }
 
     public int BerryId { get; }
     public int ItemId { get; }
     public string BerryName { get; }
     public string ItemName { get; }
+    public int ItemCost { get; }
+    public string Sprite { get; }
 }
