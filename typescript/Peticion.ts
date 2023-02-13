4cb50d0ced12cf4b290ae000004769fedc37fb71
Andres_a <andres.hoyos18@eia.edu.co>
2023-02-13 10:36:38 -0500

    .

--- typescript/Peticion.ts
+++ typescript/Peticion.ts
@@ -1,1 +1,13 @@
-import axios from "axios"
+import {axios} from "axios"
+import {Producto} from "producto"
+
+// promesas devuelven cierto tipo de dato
+(async () => {
+    async function getProductos() {
+        const {data} = await axios.get<Producto>("https://api.escuelajs.co/api/v1/products")
+        return data;
+    }
+
+    const productos = await getProductos();
+    console.log(productos)
+})()
