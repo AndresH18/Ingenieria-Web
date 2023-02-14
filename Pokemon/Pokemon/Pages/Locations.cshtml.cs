4400d67f8ae7a5d22a810924f38ba82d9c5932fc
Andres Hoyos <andres.hoyos18@eia.edu.co>
2023-02-13 23:19:27 -0500

    .

--- Pokemon/Pokemon/Pages/Locations.cshtml.cs
+++ Pokemon/Pokemon/Pages/Locations.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Pokemon.Models.ViewModels;
 using Pokemon.Services;
 
 namespace Pokemon.Pages;
@@ -13,19 +14,44 @@ public class LocationsModel : PageModel
         _service = service;
     }
 
-    public IEnumerable<string> Locations { get; set; } = Enumerable.Empty<string>();
+    public LocationsViewModel LocationsViewModel { get; set; } = new();
 
-    public async Task OnGet()
+    public int CurrentPage => LocationsViewModel.PageInfo.CurrentPage;
+    public int TotalPages => LocationsViewModel.PageInfo.TotalPages;
+
+    public IEnumerable<string> Locations => LocationsViewModel.Locations;
+
+
+    public async Task OnGet(int pageNumber, int size)
     {
+        pageNumber = pageNumber >= 1 ? pageNumber - 1 : 0;
+        size = size >= 1 ? size : 10;
+
         var region = RouteData.Values["region"]?.ToString();
         if (region != null)
         {
             // region selected, get region locations
-            Locations = await _service.GetLocations(region);
+            LocationsViewModel = await _service.GetLocations(region, size, pageNumber) ?? new LocationsViewModel();
+            // CurrentPage = LocationsViewModel.PageInfo.CurrentPage;
+            // TotalPages = (int) Math.Ceiling(Locations.Count() / (double) _pageSize);
         }
         else
         {
             // no region, get all locations
         }
+    }
+
+    public async Task OnGetPrevious()
+    {
+        await OnGet(CurrentPage - 1, LocationsViewModel.PageInfo.ItemsPerPage);
+    }
+
+    public async Task OnGetNext()
+    {
+        await OnGet(CurrentPage + 1, LocationsViewModel.PageInfo.ItemsPerPage);
+    }
+    public async Task OnGetPage(int pageNumber)
+    {
+        await OnGet(pageNumber, LocationsViewModel.PageInfo.ItemsPerPage);
     }
 }
