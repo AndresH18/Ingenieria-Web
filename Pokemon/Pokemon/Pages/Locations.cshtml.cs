772bdb752c72801e533a13de24d6b391df128e06
Andres Hoyos <andres.hoyos18@eia.edu.co>
2023-02-13 21:50:27 -0500

    Display selected Region's locations

--- Pokemon/Pokemon/Pages/Locations.cshtml.cs
+++ Pokemon/Pokemon/Pages/Locations.cshtml.cs
@@ -1,13 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Pokemon.Services;
 
 namespace Pokemon.Pages;
 
-
-
 public class LocationsModel : PageModel
 {
-    public void OnGet()
+    private readonly LocationsService _service;
+
+    public LocationsModel(LocationsService service)
     {
+        _service = service;
+    }
+
+    public IEnumerable<string> Locations { get; set; } = Enumerable.Empty<string>();
+
+    public async Task OnGet()
+    {
+        var region = RouteData.Values["region"]?.ToString();
+        if (region != null)
+        {
+            // region selected, get region locations
+            Locations = await _service.GetLocations(region);
+        }
+        else
+        {
+            // no region, get all locations
+        }
     }
 }
