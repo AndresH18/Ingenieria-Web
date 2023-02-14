a1c242a3f49dcff38b5fe7f2dc1796abd6b65f72
Andres Hoyos <andres.hoyos18@eia.edu.co>
2023-02-13 21:07:16 -0500

    Created NavigationMenu View Component

---
+++ Pokemon/Pokemon/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pokemon.Services;
+
+namespace Pokemon.Components;
+
+public class NavigationMenuViewComponent : ViewComponent
+{
+    private readonly LocationsService _service;
+
+    public NavigationMenuViewComponent(LocationsService service)
+    {
+        _service = service;
+    }
+
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        ViewBag.SelectedRegion = RouteData.Values["region"]!;
+        var regions = (await _service.GetRegions()).ToList();
+        return View(regions);
+    }
+}
